eb07bf48dbb40538560da38fe6398da98c2a845c
zhengliang <zhengliang@zhenghaosw.com>
2020-03-12 10:17:58 +0800

    springBootAdminServer

---
+++ src/main/java/com/zhsw/springbootadminserver/AdminServerApplication.java
@@ -0,0 +1,18 @@
+package com.zhsw.springbootadminserver;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhengliang
+ */
+@SpringBootApplication
+@EnableAdminServer
+public class AdminServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminServerApplication.class, args);
+    }
+
+}
